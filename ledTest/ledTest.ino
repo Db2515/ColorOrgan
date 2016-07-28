9ff4228f3c073b4422939ac851ed16f2eaa03dda
Daniel Boulby <db2515@ic.ac.uk>
2016-07-28 23:53:13 +0100

    Created test sketch to test PWM for LEDS

---
+++ ledTest/ledTest.ino
@@ -0,0 +1,24 @@
+void setup() {
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(11, OUTPUT);
+  analogReference(DEFAULT);
+
+}
+
+void loop() {
+  for(int i = 0; i <= 250; i += 25){
+     analogWrite(9, i);
+     delay(500);
+     analogWrite(9, 0);
+     
+     analogWrite(10, i);
+     delay(500);
+     analogWrite(10, 0);
+     
+     analogWrite(11, i);
+     delay(500);
+     analogWrite(11, 0);
+  } 
+
+}
