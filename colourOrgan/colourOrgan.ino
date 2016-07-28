bff69093ac100ad69675ce8499aa57807b0181cb
Daniel Boulby <db2515@ic.ac.uk>
2016-07-28 23:54:41 +0100

    Added output to LEDs
    Split the frequency bands into 3 LEDs.

--- colourOrgan/colourOrgan.ino
+++ colourOrgan/colourOrgan.ino
@@ -2,6 +2,9 @@
 int analogPin = 0;
 int strobePin = 2;
 int resetPin = 3;
+int lowPin = 9;
+int midPin = 10;
+int highPin = 11; 
 
 void setup() {
   //Open serial communication
@@ -11,6 +14,9 @@ void setup() {
   pinMode(analogPin, INPUT);
   pinMode(strobePin, OUTPUT);
   pinMode(resetPin, 3);
+  pinMode(lowPin, OUTPUT);
+  pinMode(midPin, OUTPUT);
+  pinMode(highPin, OUTPUT);
   analogReference(DEFAULT);
   
   //Ready the equaliser for use
@@ -20,21 +26,39 @@ void setup() {
 }
 
 void loop() {
-  int spectrumVolume[7];
  //Reset the equaliser to the first band
   digitalWrite(resetPin, HIGH);
   digitalWrite(resetPin, LOW);
   
+  int frequencyVolume[7];
+  
   for(int i = 0; i < 7; i++){
     //Change frequency band
     digitalWrite(strobePin, LOW);
-    delayMicroseconds(30);
+    delayMicroseconds(100);
     //Store the "volume" of the frequency
-    spectrumVolume[i] = analogRead(analogPin);
+    frequencyVolume[i] = analogRead(analogPin) * 0.6;
+    
+    switch(i){
+       case 0:
+       case 1:
+       case 2:
+         //Light the "low frequency" LED to the "frequencyVolume" 
+         analogWrite(lowPin, frequencyVolume[i]);
+         break;
+       case 3:
+       case 4:
+         analogWrite(midPin, frequencyVolume[i]);
+         break;
+       case 5:
+       case 6:
+         analogWrite(highPin, frequencyVolume[i]);
+         break;
+    }
     
     //Print the value for dubugging
     Serial.print(" ");
-    Serial.print(spectrumVolume[i]);
+    Serial.print(frequencyVolume[i]);
     
     //Move to the next band of frequencies
     digitalWrite(strobePin, HIGH);
